2fd7db1f915a1a3697352ea72231e4f38e478439
Grenbui <buidat1131@gmail.com>
2026-06-27 13:12:33 +0700

    Add validation for required attributes

--- backend/src/main/java/com/grenbui/tickets/domain/CreateTicketTypeRequest.java
+++ backend/src/main/java/com/grenbui/tickets/domain/CreateTicketTypeRequest.java
@@ -1,5 +1,8 @@
 package com.grenbui.tickets.domain;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +12,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CreateTicketTypeRequest {
 
+    @NotBlank(message = "Ticket Ttype name is required")
     private String name;
+
+    @NotNull(message = "Price is required")
+    @PositiveOrZero(message = "Price must be zero or higher")
     private Double price;
+
     private String description;
     private Integer totalAvailable;
 
